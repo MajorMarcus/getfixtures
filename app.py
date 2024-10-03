12019ec23530bf55a2fdedb006c35bad69f11b68
nemesissanto <95344083+MajorMarcus@users.noreply.github.com>
2024-10-03 16:15:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import unquote  # Import unquote to decode URLs
 
 app = Flask(__name__)
 
-def get_image_string(big_string):
-    # Extracts the image URL after 'image='
-    parts = big_string.split('image=', 1)
-    if len(parts) > 1:
-        return parts[1]
-    return None
-
 @app.route('/fixtures/<team>', methods=['GET'])
 def get_fixtures(team):
-    fixtures_list = []  # Rename to avoid conflict
+    fixtures_list = []  
     url = f'https://onefootball.com/en/team/{team}/fixtures'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -32,24 +26,23 @@ def get_fixtures(team):
             if teamname:
                 teamnames.append(teamname.text)
         
-        # Fetch team logos
+        # Fetch team logos and decode the URL
         for team in teams:
             logo = team.find('img', class_='ImageWithSets_of-image__img__pezo7')
-            logo = get_image_string(logo['src'])
             if logo:
-                logos.append(logo)  # Get the image URL
+                decoded_logo_url = unquote(logo['src'])  # Decode the logo URL
+                logos.append(decoded_logo_url)
         
         # Fetch the match date
         date = fixture.find('time', class_='title-8-bold').text if fixture.find('time', class_='title-8-bold') else "Date not found"
-        time = fixture.find('time', class_='SimpleMatchCard_simpleMatchCard__infoMessage___NJqW title-8-medium').text if fixture.find('time', class_='title-8-bold') else "Date not found"
+
         if len(teamnames) == 2 and len(logos) == 2:  # Ensure both teams and logos are found
             fixtures_list.append({
                 'team 1': teamnames[0],
                 'team 2': teamnames[1],
                 'team 1 logo': logos[0],
                 'team 2 logo': logos[1],
-                'date': date, 
-                'time': time
+                'date': date
             })
 
     return jsonify(fixtures_list)
